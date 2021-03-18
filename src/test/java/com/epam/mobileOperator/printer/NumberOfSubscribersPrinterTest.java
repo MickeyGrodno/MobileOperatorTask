5752bd9d765167f969fa84b35458aaa3e87f686d
Sergei <sergei199>
2021-03-18 16:24:47 +0300

    Изменена конфигурация тестов

--- src/test/java/com/epam/mobileOperator/printer/NumberOfSubscribersPrinterTest.java
+++ src/test/java/com/epam/mobileOperator/printer/NumberOfSubscribersPrinterTest.java
@@ -2,24 +2,30 @@ package com.epam.mobileOperator.printer;
 
 import com.epam.mobileOperator.interfaces.NumberOfSubscribersPrinter;
 import org.junit.jupiter.api.*;
+import org.mockito.ArgumentCaptor;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class NumberOfSubscribersPrinterTest {
     private static final ByteArrayOutputStream output = new ByteArrayOutputStream();
 
-    @BeforeAll
-    public static void setUpStreams() {
-        System.setOut(new PrintStream(output));
-    }
     @Test
     public void testString() throws IOException {
+        PrintStream stream = mock(PrintStream.class);
+        System.setOut(stream);
         NumberOfSubscribersPrinter numberOfSubscribersPrinter = new NumberOfSubscribersPrinterImpl();
         numberOfSubscribersPrinter.printNumberOfSubscribers();
-        String[] lineArr = output.toString().split("\\n");
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(stream).println(captor.capture());
+        String[] lineArr = captor.getValue().split("\\n");
         String actualLine = lineArr[lineArr.length-1];
         int numberOfSubscribersValue = Integer.parseInt(actualLine.replaceAll("[^0-9]", ""));
         Assertions.assertTrue(numberOfSubscribersValue>0);
+
     }
 }
